d482253bc9a64fe4e1f950fab4eed993671b9953
Nelz <nelz9999@gmail.com>
2013-02-01 17:41:50 -0800

    Staring on a level-2 cache addon.

---
+++ l2cach3/src/main/java/org/flite/cach3/level2/annotations/L2UpdateSingleCache.java
@@ -0,0 +1,12 @@
+package org.flite.cach3.level2.annotations;
+
+/**
+ * Copyright (c) 2013 Flite, Inc
+ * <p/>
+ * All rights reserved.
+ * THIS PROGRAM IS CONFIDENTIAL AND AN UNPUBLISHED WORK AND TRADE
+ * SECRET OF THE COPYRIGHT HOLDER, AND DISTRIBUTED ONLY UNDER RESTRICTION.
+ */
+
+public class L2UpdateSingleCache {
+}
